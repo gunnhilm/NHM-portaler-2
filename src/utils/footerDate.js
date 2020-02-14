f147b9e516f40daeb93dd05de013200a6b2cde6e
Eirik Rindal <eirikrindal@yahoo.no>
2020-02-14 14:45:41 +0100

    Dokumentasjon

--- src/utils/footerDate.js
+++ src/utils/footerDate.js
@@ -5,7 +5,16 @@ const getFileUpdatedDate = (samling, callback) => {
     musitFile = fileRead.setCollection(samling)
     if (fs.existsSync(musitFile)) {
         fs.stat(musitFile, function(err, stats) {
-            let time = stats.mtime.getDate() + '/' + stats.mtime.getMonth() + '-' + stats.mtime.getFullYear()
+            let time = stats.mtime
+            // .getDate() + '/' + stats.mtime.getMonth() + '-' + stats.mtime.getFullYear()
+
+            const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+            // const event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+            console.log(time);
+            
+            time = time.toLocaleString('NO', options)
+            console.log(time);
+
             callback(undefined, time)
         })
     } else {
